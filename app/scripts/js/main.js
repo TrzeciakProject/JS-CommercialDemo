d51b3c9ad24c73f1799497efceefb44eec77e725
Salthorn <patryktrzeciak1989@gmail.com>
2018-11-29 18:46:40 +0000

    Navigation bar

--- app/scripts/js/main.js
+++ app/scripts/js/main.js
@@ -3,3 +3,13 @@ console.log('Hellow World!');
 
 // This file will contain whole js code required for our single page application
 // after that - we will move to whole website creation process
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
